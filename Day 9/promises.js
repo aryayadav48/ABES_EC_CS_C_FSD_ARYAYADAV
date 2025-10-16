32bae208b70e3b73bdc3ba02ec9fc8bd39012fee
aryayadav48 <aryayadav48008@gmail.com>
2025-10-16 12:56:12 +0530

    commit

--- Day 9/promises.js
+++ Day 9/promises.js
@@ -1,37 +1,32 @@
-const orderCoffee = () => {
-    setTimeout (() => {
-        console.log("Make some order");
-        setTimeout (() => {
-            console.log("Start the machine");
-            setTimeout (() => {
-                console.log("Grinding the beans");
-                setTimeout (() => {
-                    console.log("Boiling the water");
-                   }, 1000)   
-            }, 1000)
-        }, 1000)
-    }, 4000)
-}
+// const orderCoffee = () => {
+//     setTimeout (() => {
+//         console.log("Make some order");
+//         setTimeout (() => {
+//             console.log("Start the machine");
+//             setTimeout (() => {
+//                 console.log("Grinding the beans");
+//                 setTimeout (() => {
+//                     console.log("Boiling the water");
+//                    }, 1000)   
+//             }, 1000)
+//         }, 1000)
+//     }, 4000)
+// }
 
-orderCoffee();
-
-const waitFun=(time,message) =>{
-    return new Promise(()=>{
-         setTimeout(() =>{
-        console.log(message);
-    },time)
+// orderCoffee();
+const waitfun=(time,message)=>
+{
+    return new removeEventListener((resolve)=>{
+    setTimeout(()=>(
+        console.log(message)
+    ),time)
     })
 }
-const orderCoffee =()=>{
-     waitFun(4000,"Make same order.")
-     ,then(()=> waitFun(1000,"Start the machine"))
-     ,them(()=> waitFun(1000,"Start the machine"))
-     ,then(()=> waitFun(1000,"Grinding the beans"));
+const orderCoffee=()=>{
+    waitfun(4000,"Make some order")
+    ,then(()=>waitfun(2000,"Brewing the coffee"))
+    ,then(()=>waitfun(3000,"Serving the coffee"))
+    ,then(()=>waitfun(1000,"Thank you"))
+    ;
 }
-     const orderCoffee =()=>{
-     waitFun(4000,"Make same order.");
-     await  waitFun(1000,"Start the machine")
-     await  waitFun(1000,"Start the machine");
-     await  waitFun(1000,"Grinding the beans");
-      await  waitFun(1000,"Grinding the beans");
-}
+orderCoffee();
